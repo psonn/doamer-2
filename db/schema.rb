0299c6b6d9a361c17437d4c21352b383ea9aa270
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-10-24 19:17:22 +0200

    Added names and usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024154725) do
+ActiveRecord::Schema.define(version: 20151024170946) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20151024154725) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
