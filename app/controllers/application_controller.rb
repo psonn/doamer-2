4c71db7427adc14c283b3a5a101abc7b05a4fe0b
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-11-07 23:01:04 +0100

    Added a missing pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,5 @@ protected
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :avatar) }
-    devise_parameter_sanitizer.for(:account_update) << :avatar
  end
 end
