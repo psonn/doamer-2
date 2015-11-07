a6d19636e746f12d0ebd148396889f98b61f53c0
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-11-07 21:28:56 +0100

    Added a username or email login feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,8 @@ protected
     devise_parameter_sanitizer.for(:account_update) << :last_name
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :username, uniqueness: true
   validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+      	conditions[:email].downcase! if conditions[:email]
+        where(conditions.to_hash).first
+      end
+  end
+
 end
