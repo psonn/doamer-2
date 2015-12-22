4bc594083b0f95470363fdf224dcf755a9d82e85
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-12-22 23:45:48 +0100

    Fixed some security issues

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,6 @@
 class ActivitiesController < ApplicationController
+	before_action :authenticate_user!
+
   def index
   	@activities = PublicActivity::Activity.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
   end
