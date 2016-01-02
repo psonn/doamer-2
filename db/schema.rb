d61de3383efb68f9f631a5116e446a7a71e09236
Peter Sonneveld <peter_tjuuh@hotmail.com>
2016-01-02 22:41:13 +0100

    Made profiles confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222222324) do
+ActiveRecord::Schema.define(version: 20160102213201) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -77,8 +77,12 @@ ActiveRecord::Schema.define(version: 20151222222324) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "slug"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true
