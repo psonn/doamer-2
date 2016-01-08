2652618ca6fab3acd6b8114160a704326993b82a
Peter Sonneveld <peter_tjuuh@hotmail.com>
2016-01-08 01:21:57 +0100

    Removed the follower data

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def avatar_for(user) #If you need to, pass size as a parameter or..
+	  if user.nil? or user.avatar.nil?
+	    image_tag('/images/einstein.png', :size => size) # ..you need to define size somewhere in this helper
+	  else
+	    image_tag user.avatar.public_filename(:avatar), :size => size
+	  end
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,15 +19,6 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { show: "600x600>", medium: "300x300>", thumb: "100x100>", mini: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-
   acts_as_voter
 
   validates :first_name, presence: true
@@ -54,21 +45,6 @@ class User < ActiveRecord::Base
       	conditions[:email].downcase! if conditions[:email]
         where(conditions.to_hash).first
       end
-  end
-
-  # Follows a user.
-  def follow(other_user)
-    active_relationships.create(followed_id: other_user.id)
-  end
-
-  # Unfollows a user.
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
-  end
-
-  # Returns true if the current user is following the other user.
-  def following?(other_user)
-    following.include?(other_user)
   end
 
 end
