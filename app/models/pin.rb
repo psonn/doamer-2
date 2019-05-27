b2621039c8ed2cb83259bcbe7af69f398d32c1ca
Peter Sonneveld <peter_tjuuh@hotmail.com>
2019-05-27 22:15:46 +0200

    Updated AWS settings

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	tracked owner: ->(controller, model) { controller && controller.current_user }
 
 	belongs_to :user
-	has_attached_file :image, :styles => { :show => "600x600", :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :show => "600x600", :medium => "300x300>", :thumb => "100x100>" }, s3_region: ENV["AWS_REGION"]
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 	validates_presence_of :description, :image
 
